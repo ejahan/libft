720cdc47fec6ab81015f776ab0c549521fe365bc
Elisa Jahan <elisa.jahan@free.fr>
2021-01-19 14:55:19 +0100

    norme v3

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,33 +6,44 @@
 /*   By: elisa <elisa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 15:24:46 by elisa             #+#    #+#             */
-/*   Updated: 2020/12/01 22:05:25 by elisa            ###   ########.fr       */
+/*   Updated: 2021/01/19 14:54:40 by ejahan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+static	int	ft_len_int(long n)
 {
-	char	*str;
-	int		i;
-	int		sign;
-	long	num;
+	int	len_int;
 
-	num = (n < 0) ? -(long)n : (long)n;
-	i = (n < 0) ? 2 : 1;
-	sign = (n < 0) ? -1 : 0;
-	while (num /= 10)
-		i++;
-	if (!(str = malloc(sizeof(*str) * i)))
-		return (0);
-	str[i] = '\0';
-	num = (n < 0) ? -(long)n : (long)n;
-	while (i-- + sign)
+	len_int = 0;
+	if (n < 0)
+		n = -n;
+	while (n >= 10)
 	{
-		str[i] = num % 10 + 48;
-		num = num / 10;
+		n /= 10;
+		len_int++;
 	}
-	sign == -1 ? str[0] = '-' : 0;
-	return (str);
+	return (len_int + 1);
+}
+
+char		*ft_itoa(int n)
+{
+	char	dst;
+	long	nb;
+	int		len;
+
+	nb = n;
+	len = ft_len_int(nb) + (nb < 0);
+	if ((dst = (char)malloc(sizeof(char) * len + 1 + (n < 0))) == NULL)
+		return (NULL);
+	if (nb < 0 && (nb = -nb))
+		*dst = '-';
+	dst[len--] = '\0';
+	while (len >= (n < 0))
+	{
+		dst[len--] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	return (dst);
 }
